75b30c407fa1fd879f7f3c31179b0aa0a4121df4
CAHEKrasnet <155033741+CAHEKrasnet@users.noreply.github.com>
2024-12-10 16:39:05 +0700

    Add files via upload

---
+++ mod_8_1.py
@@ -0,0 +1,15 @@
+# Задание "Программистам всё можно":
+# Домашнее задание по уроку "Try и Except"
+
+
+def add_everything_up(a, b):
+    try:
+        return round((a + b), 3)
+    except TypeError as exc:
+        return str(a) + str(b)
+
+
+print(add_everything_up(123.456, 'строка'))
+print(add_everything_up('яблоко', 4215))
+print(add_everything_up(123.456, 7))
+
